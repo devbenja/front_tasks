f5853f9331a827fe8ca10d30f732975c83adb63d
Benjamin Carías <cariasbenjamin59@gmail.com>
2024-04-29 06:03:52 -0600

    Fix CSS Conflicts

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -13,7 +13,7 @@ export const NavBar = () => {
         <nav className="bg-gray-800">
             <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
                 <div className="relative flex h-16 items-center justify-evenly">
-                    <div className="sm:block hidden flex-1 absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                    <div className="sm:block flex-1 absolute inset-y-0 right-0 hidden items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                         <h3 className="font-bold text-xl">TaskApp</h3>
                     </div>
                     <div className="flex items-center justify-center sm:items-stretch sm:justify-start">
